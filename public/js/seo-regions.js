d3b48778f0b4453ae00e590255a14d8b1779ec21
Sergey Voloshin <git@varrcan.me>
2018-05-13 04:08:21 +0300

    Fix js

--- public/js/seo-regions.js
+++ public/js/seo-regions.js
@@ -1,7 +1,5 @@
 jQuery(document).ready(function ($) {
 
-    var $popupCity = $('.seoregions__hidden')
-
     $('.seoregions__active').on('click', function (event) {
         $(this).next('.seoregions__hidden').toggleClass('seoregions__hidden-active')
         $(this).parent('.seoregions').toggleClass('seoregions__hidden-active')
@@ -9,7 +7,7 @@ jQuery(document).ready(function ($) {
     })
 
     $('body').on('click', function () {
-        $popupCity.removeClass('seoregions__hidden-active')
+        $('.seoregions__hidden').removeClass('seoregions__hidden-active')
         $('.seoregions').removeClass('seoregions__hidden-active')
     })
 
